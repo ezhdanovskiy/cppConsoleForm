11e4a101b368ade8fab134511e49b9b382dc487d
Evgeny Zhdanovskiy <7323@list.ru>
2016-09-03 12:36:46 +0300

    use pseudographic

--- main.cpp
+++ main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 
 int main() {
-    std::cout << " ____________________________________________________ " << std::endl;
-    std::cout << "|   --------------------------------     ------      |" << std::endl;
-    std::cout << "|  |[1]| mama                       |   | LOAD |     |" << std::endl;
-    std::cout << "|  |[2]| mila                       |    ------      |" << std::endl;
-    std::cout << "|  |[3]| ramu                       |                |" << std::endl;
-    std::cout << "|  |                                |    -------     |" << std::endl;
-    std::cout << "|  |                                |   | STORE |    |" << std::endl;
-    std::cout << "|  |                                |    -------     |" << std::endl;
-    std::cout << "|  |                                |                |" << std::endl;
-    std::cout << "|  |                                |    -----       |" << std::endl;
-    std::cout << "|  |                                |   | ADD |      |" << std::endl;
-    std::cout << "|  |                                |    -----       |" << std::endl;
-    std::cout << "|  |                                |                |" << std::endl;
-    std::cout << "|  |                                |    --------    |" << std::endl;
-    std::cout << "|  |                                |   | DELETE |   |" << std::endl;
-    std::cout << "|   --------------------------------     --------    |" << std::endl;
-    std::cout << "|____________________________________________________|" << std::endl;
+    std::cout << "┌────────────────────────────────────────────────────┐" << std::endl;
+    std::cout << "│  ┌────────────────────────────────┐   ╔──────╗     │" << std::endl;
+    std::cout << "│  │ mama                           │   │ LOAD │     │" << std::endl;
+    std::cout << "│  │ mila                           │   ╚──────╝     │" << std::endl;
+    std::cout << "│  │ ramu                           │                │" << std::endl;
+    std::cout << "│  │                                │   ┌───────┐    │" << std::endl;
+    std::cout << "│  │                                │   │ STORE │    │" << std::endl;
+    std::cout << "│  │                                │   └───────┘    │" << std::endl;
+    std::cout << "│  │                                │                │" << std::endl;
+    std::cout << "│  │                                │   ┌─────┐      │" << std::endl;
+    std::cout << "│  │                                │   │ ADD │      │" << std::endl;
+    std::cout << "│  │                                │   └─────┘      │" << std::endl;
+    std::cout << "│  │                                │                │" << std::endl;
+    std::cout << "│  │                                │   ┌────────┐   │" << std::endl;
+    std::cout << "│  │                                │   │ DELETE │   │" << std::endl;
+    std::cout << "│  └────────────────────────────────┘   └────────┘   │" << std::endl;
+    std::cout << "└────────────────────────────────────────────────────┘" << std::endl;
     return 0;
 }
